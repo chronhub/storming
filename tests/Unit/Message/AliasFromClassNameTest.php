65cc60c36e1072e3bea530021c6a6985344cf6ef
steph bug <chronhubgit@gmail.com>
2023-09-01 14:30:14 +0200

    work(package): wip

--- tests/Unit/Message/AliasFromClassNameTest.php
+++ tests/Unit/Message/AliasFromClassNameTest.php
@@ -5,19 +5,23 @@ declare(strict_types=1);
 namespace Storm\Tests\Unit\Message;
 
 use Storm\Message\AliasFromClassName;
+use Storm\Message\InvalidMessageAlias;
 
-it('should return alias from class name', function () {
+it('return alias from class name', function () {
     $alias = new AliasFromClassName();
+
     expect($alias->toAlias(AliasFromClassName::class))->toBe(AliasFromClassName::class);
 });
 
-it('should return alias from object', function () {
+it('return alias from object', function () {
     $alias = new AliasFromClassName();
+
     expect($alias->toAlias(new AliasFromClassName()))->toBe(AliasFromClassName::class);
 });
 
-it('raise exception when class does not exists', function () {
+it('raises exception when class does not exists', function () {
     $alias = new AliasFromClassName();
+
     /** @phpstan-ignore-next-line */
     $alias->toAlias('InvalidClass');
-})->throws('Storm\Message\InvalidMessageAlias', 'Message class name InvalidClass does not exists.');
+})->throws(InvalidMessageAlias::class, 'Message class name InvalidClass does not exists.');
--- tests/Unit/Message/AliasFromMapTest.php
+++ tests/Unit/Message/AliasFromMapTest.php
@@ -6,6 +6,7 @@ namespace Storm\Tests\Unit\Message;
 
 use Storm\Message\AliasFromClassName;
 use Storm\Message\AliasFromMap;
+use Storm\Message\InvalidMessageAlias;
 
 dataset('map', [
     [
@@ -13,24 +14,27 @@ dataset('map', [
     ],
 ]);
 
-it('should return alias with string class name from map', function (array $map) {
-
+it('return alias with string class name from map', function (array $map) {
     $alias = new AliasFromMap($map);
+
     expect($alias->toAlias(AliasFromClassName::class))->toBe('alias-from-map');
 })->with('map');
 
-it('should return alias with object from map', function (array $map) {
+it('return alias with object from map', function (array $map) {
     $alias = new AliasFromMap($map);
+
     expect($alias->toAlias(new AliasFromClassName()))->toBe('alias-from-map');
 })->with('map');
 
-it('raise exception when class does not exists', function () {
+it('raises exception when class does not exists', function () {
     $alias = new AliasFromMap([]);
+
     /** @phpstan-ignore-next-line */
     $alias->toAlias('InvalidClass');
-})->throws('Storm\Message\InvalidMessageAlias', 'Message class name InvalidClass does not exists.');
+})->throws(InvalidMessageAlias::class, 'Message class name InvalidClass does not exists.');
 
-it('raise exception when alias does not exists in map', function () {
+it('raises exception when alias does not exists in map', function () {
     $alias = new AliasFromMap([]);
+
     $alias->toAlias(AliasFromClassName::class);
-})->throws('Storm\Message\InvalidMessageAlias', 'Message class name Storm\Message\AliasFromClassName not found in map.');
+})->throws(InvalidMessageAlias::class, 'Message class name Storm\Message\AliasFromClassName not found in map.');
