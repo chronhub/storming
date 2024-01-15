4e4cc1b9a71b9f93dbbaadcc58ba26aa1fee684c
steph bug <chronhubgit@gmail.com>
2024-01-15 11:41:20 +0100

    fix(composer): bump

--- bin/split.sh
+++ bin/split.sh
@@ -18,22 +18,22 @@ function remote()
 
 git pull origin $CURRENT_BRANCH
 
-remote contract git@github.com:chronhub/contract.git
-remote stream git@github.com:chronhub/stream.git
-remote message git@github.com:chronhub/message.git
-remote tracker git@github.com:chronhub/tracker.git
 remote clock git@github.com:chronhub/clock.git
-remote reporter git@github.com:chronhub/reporter.git
+remote contract git@github.com:chronhub/contract.git
 remote chronicler git@github.com:chronhub/chronicler.git
+remote message git@github.com:chronhub/message.git
+remote reporter git@github.com:chronhub/reporter.git
 remote serializer git@github.com:chronhub/serializer.git
+remote stream git@github.com:chronhub/stream.git
 remote support git@github.com:chronhub/support.git
+remote tracker git@github.com:chronhub/tracker.git
 
-split 'src/Storm/Contract' contract
-split 'src/Storm/Stream' stream
-split 'src/Storm/Message' message
-split 'src/Storm/Tracker' tracker
 split 'src/Storm/Clock' clock
-split 'src/Storm/Reporter' reporter
+split 'src/Storm/Contract' contract
 split 'src/Storm/Chronicler' chronicler
+split 'src/Storm/Message' message
+split 'src/Storm/Reporter' reporter
 split 'src/Storm/Serializer' serializer
+split 'src/Storm/Stream' stream
 split 'src/Storm/Support' support
+split 'src/Storm/Tracker' tracker
