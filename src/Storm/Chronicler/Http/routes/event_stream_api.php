bfb3c677c4df873fbc1fe2180db39befec5c3e23
steph bug <chronhubgit@gmail.com>
2024-03-26 12:32:10 +0100

    fix routes

--- src/Storm/Chronicler/Http/routes/event_stream_api.php
+++ src/Storm/Chronicler/Http/routes/event_stream_api.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Storm\Chronicler\Http\Controllers\EventStream\CreateEventStreamApi;
-use Storm\Chronicler\Http\Controllers\EventStream\RequestEventStreamExists;
+use Storm\Chronicler\Http\Controllers\EventStream\RequestEventStreamExistsApi;
 
 Route::group(['prefix' => 'event-stream'], function () {
-    Route::get('/', RequestEventStreamExists::class);
+    Route::get('/', RequestEventStreamExistsApi::class);
 
     Route::post('/', CreateEventStreamApi::class);
 });
