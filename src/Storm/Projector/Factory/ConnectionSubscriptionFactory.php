5887d700535a50cca388d9538505db3d291da0e3
steph bug <chronhubgit@gmail.com>
2024-06-17 16:25:39 +0200

    wip checkpoints

--- src/Storm/Projector/Factory/ConnectionSubscriptionFactory.php
+++ src/Storm/Projector/Factory/ConnectionSubscriptionFactory.php
@@ -5,14 +5,8 @@ declare(strict_types=1);
 namespace Storm\Projector\Factory;
 
 use Override;
-use Storm\Contract\Projector\CheckpointRecognition;
 use Storm\Contract\Projector\ProjectionOption;
 use Storm\Contract\Projector\ProjectionRepository;
-use Storm\Projector\Checkpoint\CheckpointCollection;
-use Storm\Projector\Checkpoint\CheckpointStore;
-use Storm\Projector\Checkpoint\GapRules;
-use Storm\Projector\Checkpoint\NoopGapDetector;
-use Storm\Projector\Checkpoint\ReadOnlyCheckpointStore;
 use Storm\Projector\Repository\InMemoryRepository;
 
 final class ConnectionSubscriptionFactory extends AbstractSubscriptionFactory
@@ -40,21 +34,5 @@ final class ConnectionSubscriptionFactory extends AbstractSubscriptionFactory
         }
 
         return $this->createDispatcherRepository($repository);
-    }
-
-    #[Override]
-    protected function createCheckpointRecognition(ProjectionOption $option, bool $detectGap): CheckpointRecognition
-    {
-        $checkpoints = new CheckpointCollection($this->clock);
-
-        if ($detectGap) {
-            return new CheckpointStore(
-                $checkpoints,
-                new NoopGapDetector(), // fixMe
-                new GapRules()
-            );
-        }
-
-        return new ReadOnlyCheckpointStore($checkpoints);
     }
 }
