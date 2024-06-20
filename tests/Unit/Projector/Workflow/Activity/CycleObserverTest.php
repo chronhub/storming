c6a29a60f522541dffbee4a30788c6496e79a2e3
steph bug <chronhubgit@gmail.com>
2024-06-20 13:00:30 +0200

    tests

---
+++ tests/Unit/Projector/Workflow/Activity/CycleObserverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Storm\Tests\Unit\Projector\Workflow\Activity;
+
+use Storm\Contract\Projector\NotificationHub;
+use Storm\Projector\Workflow\Activity\CycleObserver;
+use Storm\Projector\Workflow\Notification\Cycle\CycleBegan;
+use Storm\Projector\Workflow\Notification\Cycle\CycleRenewed;
+use Storm\Projector\Workflow\Notification\Sprint\IsSprintTerminated;
+
+beforeEach(function () {
+    $this->hub = mock(NotificationHub::class);
+    $this->activity = new CycleObserver();
+});
+
+test('start and end cycle and return boolean depends on is sprint is terminated', function (bool $isSprintTerminated) {
+    $this->hub->shouldReceive('notify')->with(CycleBegan::class)->once();
+    $this->hub->shouldReceive('notify')->with(CycleRenewed::class)->once();
+    $this->hub->shouldReceive('expect')->with(IsSprintTerminated::class)->once()->andReturn($isSprintTerminated);
+
+    $keepRunning = ($this->activity)($this->hub, fn ($hub) => null);
+
+    expect($keepRunning)->toBe(! $isSprintTerminated);
+})->with([
+    'sprint is not terminated' => fn () => false,
+    'sprint is terminated' => fn () => true,
+]);
