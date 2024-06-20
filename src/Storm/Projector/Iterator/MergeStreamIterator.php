850e4390e9bcd69f83c538f0cc137fb8bad3ecb6
steph bug <chronhubgit@gmail.com>
2024-06-20 15:59:08 +0200

    doc

--- src/Storm/Projector/Iterator/MergeStreamIterator.php
+++ src/Storm/Projector/Iterator/MergeStreamIterator.php
@@ -75,6 +75,10 @@ final class MergeStreamIterator implements Countable, Iterator
     }
 
     /**
+     * Returns the total number of events in all streams.
+     * The count decreases as events are iterated over,
+     * unless rewind() is called.
+     *
      * @phpstan-impure
      */
     public function count(): int
