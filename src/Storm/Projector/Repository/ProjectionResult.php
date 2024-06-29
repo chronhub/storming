54e54c98f1e124eb9960a055cf0a61e273433272
steph bug <chronhubgit@gmail.com>
2024-06-29 07:34:11 +0200

    doc

--- src/Storm/Projector/Repository/ProjectionResult.php
+++ src/Storm/Projector/Repository/ProjectionResult.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Storm\Projector\Repository;
 
+use Storm\Projector\Checkpoint\Checkpoint;
+
 final readonly class ProjectionResult
 {
+    /**
+     * @param array<string, Checkpoint> $checkpoints
+     */
     public function __construct(
         public array $checkpoints,
         public array $userState,
