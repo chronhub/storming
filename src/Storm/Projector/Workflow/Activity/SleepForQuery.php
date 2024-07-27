2b9886f956a69c64c29525592ab401383e191a6b
steph bug <chronhubgit@gmail.com>
2024-07-27 14:44:21 +0200

    sleep for query when no gap

--- src/Storm/Projector/Workflow/Activity/SleepForQuery.php
+++ src/Storm/Projector/Workflow/Activity/SleepForQuery.php
@@ -6,12 +6,15 @@ namespace Storm\Projector\Workflow\Activity;
 
 use Storm\Contract\Projector\NotificationHub;
 use Storm\Projector\Workflow\Notification\Command\BatchStreamSleep;
+use Storm\Projector\Workflow\Notification\Promise\HasGap;
 
 final readonly class SleepForQuery
 {
     public function __invoke(NotificationHub $hub, callable $next): callable|bool
     {
-        $hub->emit(BatchStreamSleep::class);
+        $hub->emitWhen(! $hub->await(HasGap::class), function (NotificationHub $hub) {
+            $hub->emit(BatchStreamSleep::class);
+        });
 
         return $next($hub);
     }
