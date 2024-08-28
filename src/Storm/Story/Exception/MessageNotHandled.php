994ea670f19fed74b673ed30365ba575536c8311
steph bug <chronhubgit@gmail.com>
2024-08-28 11:39:23 +0200

    minor(story): set unhandled message on exception

--- src/Storm/Story/Exception/MessageNotHandled.php
+++ src/Storm/Story/Exception/MessageNotHandled.php
@@ -8,8 +8,17 @@ use function get_class;
 
 class MessageNotHandled extends StoryException
 {
+    protected static ?object $unhandledMessage = null;
+
     public static function withMessage(object $message): self
     {
+        self::$unhandledMessage = $message;
+
         return new self('Message not handled: '.get_class($message));
+    }
+
+    public function getUnhandledMessage(): ?object
+    {
+        return self::$unhandledMessage;
     }
 }
